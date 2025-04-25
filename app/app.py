7819f283f803213afb041d8ccd973f74927b8156
matteovar <matvarnier@gmail.com>
2025-04-25 15:36:28 -0300

    update

--- app/app.py
+++ app/app.py
@@ -111,4 +111,5 @@ def remover_gasto(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
